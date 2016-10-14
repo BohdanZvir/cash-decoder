6628f0ede0b18f128a07baf922be1ab4b8b3d4a8
bohdan <zvirbohdan@gmail.com>
2016-10-14 22:36:11 +0300

    extract as constant application name added option branch to generate classes

--- src/main/java/com/bzvir/util/Decoder.java
+++ src/main/java/com/bzvir/util/Decoder.java
@@ -9,11 +9,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.cli.*;
 import org.unsynchronized.jdeserialize;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+
 public class Decoder {
+
+    public static final String APPLICATION_NAME = "cash-with-p24";
 
     private static void printJsonValue(Object object) {
         ObjectMapper mapper = new ObjectMapper();
@@ -46,7 +51,7 @@ public class Decoder {
         args.add(filePath);
 
         ConsoleOutputCapturer capturer = new ConsoleOutputCapturer();
-        capturer.start(false);
+        capturer.start(true);
         jdeserialize.main(args.toArray(new String[]{}));
         String textline = capturer.stop();
 //        System.out.println(" ============ hello from here ");
@@ -58,28 +63,41 @@ public class Decoder {
         Options options = new Options();
         options.addOption("help", false, "display help");
         options.addOption("p24", false, "use Privat24 xls file as source");
+        options.addOption("generate", true,
+                "[source] [target] generate classes from source path and put it into target path");
 
         CommandLine cmd = new DefaultParser().parse(options, args);
 
         boolean cashAsSource;
 
+        Decoder decoder = new Decoder();
         if(cmd.hasOption("help")) {
             HelpFormatter formatter = new HelpFormatter();
-            formatter.printHelp("cash-with-p24", options);
-            return;
+            formatter.printHelp(APPLICATION_NAME, options);
+        } else if (cmd.hasOption("generate")) {
+            String[] values = cmd.getOptionValues("generate");
+            if (values == null || values.length == 0) {
+                throw new RuntimeException("There are no source and no target path");
+            } else if (values.length == 1) {
+                decoder.doWork_2(values[0], values[0]);
+            } else {
+                decoder.doWork_2(values[0], values[1]);
+            }
         } else {
             cashAsSource = !cmd.hasOption("p24");
+            decoder.doWork(cashAsSource);
         }
+    }
 
-        new Decoder().doWork(cashAsSource);
-
-
-
-//        jDeserial(dirPath + "settings.dat");
-//        String dataFilePath = dirPath + "settings.dat";
-//        ClassGenerator generator = new ClassGenerator();
-//        Class loadedClass = generator.generateClassDeclarations(dataFilePath);
-
+    private void doWork_2(String source, String target) {
+        if (isNullOrEmpty(source)) {
+            source = System.getProperty("user.dir") + "/sample data/";
+        }
+        if (isNullOrEmpty(target)) {
+            target = source;
+        }
+        ClassGenerator generator = new ClassGenerator();
+        generator.constructClasses(source, new File(target));
     }
 
     public void doWork(boolean cashIsSource) {
