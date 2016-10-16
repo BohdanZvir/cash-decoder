9453975a7a1a5e0bc8b7cc06ad8d706755f72d5a
bohdan <zvirbohdan@gmail.com>
2016-10-16 23:17:38 +0300

    fixed encoding issue (ISO_8859_1 - > UTF_8)

--- src/main/java/com/bzvir/reader/Privat24ResourceUtil.java
+++ src/main/java/com/bzvir/reader/Privat24ResourceUtil.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 /**
  * Created by root.
  */
@@ -20,10 +23,15 @@ public class Privat24ResourceUtil {
         int i = 0;
         while (bundle.containsKey("p24.title." + i)) {
             String title = bundle.getString("p24.title." + i);
-            titles.add(title);
+            titles.add(toUTF8(title));
             i++;
         }
         return titles;
+    }
+
+    private String toUTF8(String title) {
+        byte ptext[] = title.getBytes(ISO_8859_1);
+        return new String(ptext, UTF_8);
     }
 
 }
