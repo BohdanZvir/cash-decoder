065d4a94d75418fef644f187541fc3a133654517
bohdan <zvirbohdan@gmail.com>
2016-10-22 19:29:06 +0300

    reverted default properties from Event

--- src/main/java/com/bzvir/model/Event.java
+++ src/main/java/com/bzvir/model/Event.java
@@ -12,10 +12,7 @@ import java.util.Map;
 @ToString
 @EqualsAndHashCode
 public class Event {
-    private final static String CATEGORY = "Category";
-    public static final String DATE = "Date";
-    public static final String AMOUNT = "Amount";
-    public static final String DESCRIPTION = "Description";
+    private final static String CATEGORY = "Категорія";
     private Map<String, Object> data;
 
     public Event() {
@@ -28,30 +25,6 @@ public class Event {
 
     public String getCategory() {
         return data.containsKey(CATEGORY) ? (String) data.get(CATEGORY) : "";
-    }
-
-    public void setDate(String date) {
-        data.put(DATE, date);
-    }
-
-    public String getDate() {
-        return data.containsKey(DATE) ? (String) data.get(DATE) : "";
-    }
-
-    public void setAmount(int amount) {
-        data.put(AMOUNT, amount);
-    }
-
-    public int getAmount() {
-        return data.containsKey(AMOUNT) ? (int) data.get(AMOUNT) : 0;
-    }
-
-    public void setDescription(String description) {
-        data.put(DESCRIPTION, description);
-    }
-
-    public String getDescription() {
-        return data.containsKey(DESCRIPTION) ? (String) data.get(DESCRIPTION) : "";
     }
 
     public void setProperty(String key, Object value) {
